284ef7415a300c3f6dc411182e856df8ce3526bb
Jennifer Kerr <jkerr321@gmail.com>
2019-10-06 13:04:38 +0200

    use env vars for config

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const GoogleSpreadsheet = require('google-spreadsheet');
 const { promisify } = require('util');
-
-const creds = require('./client_secret.json');
+const dotenv = require('dotenv');
+dotenv.config();
+const config = require('./config');
 
 async function accessSpreadsheet () {
-    const doc = new GoogleSpreadsheet('1Khj2u55fpyr7pjKxJKu8HQzmj6UD5x2fTAxpsme0wcM');
-    await promisify(doc.useServiceAccountAuth)(creds);
-    const info = await promisify(doc.getInfo)();
-    const sheet = info.worksheets[0];
-    const cells = await promisify(sheet.getCells)({
-        "min-row": 2,
-        "max-row": 300,
-        "min-col": 19,
-        "max-col": 27 
-    });
-    console.log(cells);
+    try {
+        const doc = new GoogleSpreadsheet('1Khj2u55fpyr7pjKxJKu8HQzmj6UD5x2fTAxpsme0wcM');
+        await promisify(doc.useServiceAccountAuth)(config);
+        const info = await promisify(doc.getInfo)();
+        const sheet = info.worksheets[0];
+        const cells = await promisify(sheet.getCells)({
+            "min-row": 2,
+            "max-row": 300,
+            "min-col": 19,
+            "max-col": 27
+        });
+        console.log(cells);
+    } catch (err) {
+        console.log('==================');
+        console.log('error', err);
+        console.log('==================');
+    }
 }
 
 accessSpreadsheet();
