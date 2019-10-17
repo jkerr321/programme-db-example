d62ca4f3b1692ade9fef9ecebda6236cd3d64ad6
Jennifer Kerr <jkerr321@gmail.com>
2019-10-17 20:42:52 +0100

    adding some styling for grid

--- server/controllers/renderLandingPage.js
+++ server/controllers/renderLandingPage.js
@@ -6,9 +6,9 @@ const config = require('../../config');
 
 module.exports = async (req, res) => {
     try {
-        const value = await accessSpreadsheet();
+        const gridItems = await accessSpreadsheet();
 
-        return res.render('landing', { value });
+        return res.render('landing', { gridItems });
     } catch (err) {
         console.log('==================');
         console.log('render error', err);
@@ -19,18 +19,21 @@ module.exports = async (req, res) => {
 
 async function accessSpreadsheet() {
     try {
-        const doc = new GoogleSpreadsheet('1Khj2u55fpyr7pjKxJKu8HQzmj6UD5x2fTAxpsme0wcM');
-        await promisify(doc.useServiceAccountAuth)(config);
-        const info = await promisify(doc.getInfo)();
-        const sheet = info.worksheets[0];
-        const cells = await promisify(sheet.getCells)({
-            "min-row": 2,
-            "max-row": 300,
-            "min-col": 19,
-            "max-col": 27
-        });
-        const one = JSON.stringify(cells[0]);
-        return one;
+        // const doc = new GoogleSpreadsheet('1Khj2u55fpyr7pjKxJKu8HQzmj6UD5x2fTAxpsme0wcM');
+        // await promisify(doc.useServiceAccountAuth)(config);
+        // const info = await promisify(doc.getInfo)();
+        // const sheet = info.worksheets[0];
+        // const cells = await promisify(sheet.getCells)({
+        //     "min-row": 2,
+        //     "max-row": 300,
+        //     "min-col": 19,
+        //     "max-col": 27
+        // });
+        // const one = JSON.stringify(cells[0]);
+        return {
+            A1: "blue",
+            B1: "orange"
+         };
     } catch (err) {
         console.log('==================');
         console.log('error', err);
