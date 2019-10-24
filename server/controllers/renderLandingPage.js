73ea916e328c4be2aeb37692375212dbade46245
Jennifer Kerr <jkerr321@gmail.com>
2019-10-24 17:43:15 +0100

    heroku google auth debugging

--- server/controllers/renderLandingPage.js
+++ server/controllers/renderLandingPage.js
@@ -2,6 +2,14 @@ const GoogleSpreadsheet = require('google-spreadsheet');
 const { promisify } = require('util');
 const config = require('../../config');
 
+console.log('==================');
+console.log('config.private_key', config.private_key);
+console.log('==================');
+console.log('==================');
+console.log('config.private_key_her', config.private_key_her);
+console.log('==================');
+
+
 module.exports = async (req, res) => {
     try {
         const gridItems = await accessSpreadsheet();
