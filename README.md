a1597c3d8efac61a7f9046afe8d14d3799481c16
Jennifer Kerr <jkerr321@gmail.com>
2019-11-27 14:06:18 +0000

    connect to DB spreadsheet and initial render

--- README.md
+++ README.md
@@ -1,31 +1,15 @@
 TODO
 
-Tidy up
+- draw grids
+- create routes
+- front page
+- season page
+- Info modal
+- Form modal
+- Set up website
+- Get favicon
 - readme
 - From - Actually fill notes section with notes
 - Password for edit mode?
 - add older gallery
 - get off hobby Dyno
-- responsive gallery
-
-SpreadsheetRow
-Represents a single row from a sheet.
-
-These are returned from calling GoogleSpreadsheet.getRows and SpreadsheetWorksheet.getRows.
-
-You can treat the row as a normal javascript object. Object keys will be from the header row of your sheet, however the google API mangles the names a bit to make them simpler. It's easiest if you just use all lowercase keys to begin with.
-
-See limitations above for notes about Google's row-based API!
-
-SpreadsheetRow.save( callback )
-Saves any changes made to the row's values.
-
-SpreadsheetRow.del( callback )
-Deletes the row from the sheet.
-
-Warnings that might need dealing with: 
-
-A cookie associated with a cross-site resource at http://rhs.org.uk/ was set without the `SameSite` attribute. A future release of Chrome will only deliver cookies with cross-site requests if they are set with `SameSite=None` and `Secure`. You can review cookies in developer tools under Application>Storage>Cookies and see more details at https://www.chromestatus.com/feature/5088147346030592 and https://www.chromestatus.com/feature/5633521622188032.
-
-
-The connection used to load resources from https://apps.rhs.org.uk used TLS 1.0 or TLS 1.1, which are deprecated and will be disabled in the future. Once disabled, users will be prevented from loading these resources. The server should enable TLS 1.2 or later. See https://www.chromestatus.com/feature/5654791610957824 for more information.
--- server/controllers/renderLandingPage.js
+++ server/controllers/renderLandingPage.js
@@ -5,14 +5,112 @@ const { promisify } = require('util');
 const config = require('../../config');
 const colours = require('../colours');
 
-const getRows = async (season) => {
+// const getRows = async (season) => {
+//     try {
+//         const doc = new GoogleSpreadsheet('1igrPkooUEO7EhljF3tjjuuZf67smjO9obRVngghhYu8');
+//         await promisify(doc.useServiceAccountAuth)(config);
+//         const info = await promisify(doc.getInfo)();
+
+//         const sheet = info.worksheets.reduce((acc, worksheet, i) => {
+//             if (worksheet.title.toLowerCase() === season.toLowerCase()) {
+//                 acc = worksheet;
+//             }
+//             return acc;
+//         }, {});
+
+//         const rows = await promisify(sheet.getRows)({
+//             "offset": 1,
+//             "limit": 300
+//         });        
+//         return rows;
+
+//     } catch (err) {
+//         console.log('accessSpreadsheet error', err);
+//     }
+// }
+
+// const updateSpreadsheet = async (rows, reqBody) => {
+//     try {
+//         rows.forEach(async row => {
+//             if (row.position === reqBody.position) {
+//                 Object.keys(reqBody).forEach(key => {
+//                     if (reqBody.removePlant) {
+//                         // remove all values, then put position value back so it still renders as an empty spot on the grid next time round
+//                         row[key] = '';
+//                         row.position = reqBody.position;
+//                     } else if (reqBody[key]) {
+//                         // e.g. if (reqBody.colour) {row.colour = reqBody.colour};
+//                         row[key] = reqBody[key]
+//                     }
+//                 });
+//                 await row.save();
+//             }
+//         })
+//         return 
+        
+//     } catch (err) {
+//         console.log('updateSpreadsheet error', err);
+//     }
+// }
+
+// const getPlantData = rows => {
+//     try {
+//         const values = rows.reduce((acc, row) => {
+//             if (row.position) {
+//                 acc.push({
+//                     isDecking: `${row.position}`.includes('17') || `${row.position}`.includes('18') || `${row.position}`.includes('19'),
+//                     position: row.position,
+//                     commonName: row.commonname,
+//                     latinName: row.latinname,
+//                     perennialAnnual: `${row.perennialannual}`.substr(0, 1),
+//                     plantedDate: row.planteddate,
+//                     floweringPeriod: row.floweringperiod,
+//                     colour: row.colour,
+//                     image: row.image,
+//                     link: row.link,
+//                     isFilled: !!row.filled,
+//                     notes: row.notes
+//                 });
+//             }
+//             return acc;
+//         }, []);
+//         values.forEach(value => {
+//             if (value.position === 'R5') {
+//             }
+//         })
+//         return values;
+//     } catch (err) {
+//         console.log('getPlantData error', err);
+//     }
+// }
+
+// module.exports = async (req, res, seasonName) => {
+//     try {
+//         const seasonPath = req.path.substr(1); // e.g. 'summer19'
+//         const rows = await getRows('Full List');
+//         let gridItems;
+//         if (req.method === "POST") {
+//             await updateSpreadsheet(rows, req.body);
+//             const updatedRows = await getRows(seasonPath)
+//             gridItems = await getPlantData(updatedRows);
+//         } else {
+//             gridItems = await getPlantData(rows);
+//         }
+//         return res.render('landing', { rows });
+//     } catch (err) {
+//         console.log('render error', err);
+//     };
+// };
+
+module.exports = async (req, res, seasonName) => {
     try {
-        const doc = new GoogleSpreadsheet('1Khj2u55fpyr7pjKxJKu8HQzmj6UD5x2fTAxpsme0wcM');
+        const doc = new GoogleSpreadsheet('1igrPkooUEO7EhljF3tjjuuZf67smjO9obRVngghhYu8');
         await promisify(doc.useServiceAccountAuth)(config);
         const info = await promisify(doc.getInfo)();
 
+        //TODO delete other sheets so this is the only one we have so we can delete this code
         const sheet = info.worksheets.reduce((acc, worksheet, i) => {
-            if (worksheet.title.toLowerCase() === season.toLowerCase()) {
+            if (worksheet.title === 'FullList') {
                 acc = worksheet;
             }
             return acc;
@@ -20,83 +118,10 @@ const getRows = async (season) => {
 
         const rows = await promisify(sheet.getRows)({
             "offset": 1,
-            "limit": 300
-        });        
-        return rows;
+            "limit": 5000
+        });
 
-    } catch (err) {
-        console.log('accessSpreadsheet error', err);
-    }
-}
-
-const updateSpreadsheet = async (rows, reqBody) => {
-    try {
-        rows.forEach(async row => {
-            if (row.position === reqBody.position) {
-                Object.keys(reqBody).forEach(key => {
-                    if (reqBody.removePlant) {
-                        // remove all values, then put position value back so it still renders as an empty spot on the grid next time round
-                        row[key] = '';
-                        row.position = reqBody.position;
-                    } else if (reqBody[key]) {
-                        // e.g. if (reqBody.colour) {row.colour = reqBody.colour};
-                        row[key] = reqBody[key]
-                    }
-                });
-                await row.save();
-            }
-        })
-        return 
-        
-    } catch (err) {
-        console.log('updateSpreadsheet error', err);
-    }
-}
-
-const getPlantData = rows => {
-    try {
-        const values = rows.reduce((acc, row) => {
-            if (row.position) {
-                acc.push({
-                    isDecking: `${row.position}`.includes('17') || `${row.position}`.includes('18') || `${row.position}`.includes('19'),
-                    position: row.position,
-                    commonName: row.commonname,
-                    latinName: row.latinname,
-                    perennialAnnual: `${row.perennialannual}`.substr(0, 1),
-                    plantedDate: row.planteddate,
-                    floweringPeriod: row.floweringperiod,
-                    colour: row.colour,
-                    image: row.image,
-                    link: row.link,
-                    isFilled: !!row.filled,
-                    notes: row.notes
-                });
-            }
-            return acc;
-        }, []);
-        values.forEach(value => {
-            if (value.position === 'R5') {
-            }
-        })
-        return values;
-    } catch (err) {
-        console.log('getPlantData error', err);
-    }
-}
-
-module.exports = async (req, res, seasonName) => {
-    try {
-        const seasonPath = req.path.substr(1); // e.g. 'summer19'
-        const rows = await getRows(seasonPath);
-        let gridItems;
-        if (req.method === "POST") {
-            await updateSpreadsheet(rows, req.body);
-            const updatedRows = await getRows(seasonPath)
-            gridItems = await getPlantData(updatedRows);
-        } else {
-            gridItems = await getPlantData(rows);
-        }
-        return res.render('landing', { gridItems, seasonName, seasonPath, colours });
+        return res.render('landing', { rows });
     } catch (err) {
         console.log('render error', err);
     };
